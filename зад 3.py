72ead4289be3de6d5c874e32720929ea0c775de1
Katysha <Milaya-katyscha@mail.ru>
2022-09-19 22:34:36 +0300

    Пепеляева зад 1

---
+++ зад 3.py
@@ -0,0 +1,27 @@
+#a=морожное с добавкой
+#b=морожное без добавки
+#f=добавка к мороженому
+class IceCream:
+    def __init__(self,f):
+       # self.f=f
+    #def ret(self):
+        if isinstance(f,str):
+            self.f=f
+        else:
+            self.f=None
+
+
+    def info_about_icecream(self):
+        if self.f:
+            print(f"мороженое и {self.f}")
+        else:
+            print("обычное мороженое")
+
+
+
+a1=IceCream("черника")
+a2=IceCream("малина")
+a3=IceCream(4)
+a1.info_about_icecream()
+a2.info_about_icecream()
+a3.info_about_icecream()
